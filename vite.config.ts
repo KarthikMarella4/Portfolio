748a2b01a7366209484569f5f356b95fb5c093bc
KarthikMarella4 <karthikmarella24@gmail.com>
2025-11-20 16:50:23 +0530

    Fix index.css path and Vite base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/KarthikMarella4-Karthik-Marella-Portfolio/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
+  build: { outDir: 'dist' },
 });
