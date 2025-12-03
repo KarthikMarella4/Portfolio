3817adfa0e0f2c9d9e394711ff64d448bb16f305
KarthikMarella4 <karthikmarella24@gmail.com>
2025-12-03 20:46:42 +0530

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Karthik-Marella-Portfolio/',   // 👈 IMPORTANT
+  base: '/Portfolio/',   // 👈 must match repo name exactly (case-sensitive)
   plugins: [react()],
 })
